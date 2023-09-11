3ff5c7cf3af99410c82e1f3da4ed80ad6dbb5a8b
Stefan Boos <sboos@igus.net>
2023-09-11 16:29:41 +0200

    fix: count_cards_in_image parameter was missing

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ from iterative_metrics.count_cards_in_image import count_cards_in_image
 
 
 if __name__ == "__main__":
-    count_cards_in_image()
+    count_cards_in_image("")
     cumulative_flow_diagram()
