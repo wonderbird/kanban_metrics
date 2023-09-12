c658dbaa4781c511077d8954b867b29300f455a1
Stefan Boos <kontakt@boos.systems>
2023-09-12 08:33:29 +0200

    feat(count_cards): display 1 card as black/white image

--- src/iterative_metrics/count_cards_in_image.py
+++ src/iterative_metrics/count_cards_in_image.py
@@ -1,2 +1,18 @@
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+
 def count_cards_in_image(path):
+    original_image = cv.imread(
+        "../../tests/data/1_kanban_cards_width_140px_height_100px_resolution_144dpi.png"
+    )
+
+    hsv_image = cv.cvtColor(original_image, cv.COLOR_RGB2HSV)
+    black_white_image = cv.inRange(hsv_image, (0, 0, 100), (0, 0, 255))
+    plt.imshow(black_white_image, cmap="gray")
+    plt.show()
     return 0
+
+
+if __name__ == "__main__":
+    count_cards_in_image("")
