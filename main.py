b2ba11d70deb2c3419c2b39d97ed9e6c45127d0b
Stefan Boos <kontakt@boos.systems>
2023-09-12 08:35:21 +0200

    fix(count_cards): correct image path

--- main.py
+++ main.py
@@ -5,5 +5,7 @@ from iterative_metrics.count_cards_in_image import count_cards_in_image
 
 
 if __name__ == "__main__":
-    count_cards_in_image("")
+    count_cards_in_image(
+        "tests/data/1_kanban_cards_width_140px_height_100px_resolution_144dpi.png"
+    )
     cumulative_flow_diagram()
--- src/iterative_metrics/count_cards_in_image.py
+++ src/iterative_metrics/count_cards_in_image.py
@@ -3,9 +3,7 @@ import matplotlib.pyplot as plt
 
 
 def count_cards_in_image(path):
-    original_image = cv.imread(
-        "../../tests/data/1_kanban_cards_width_140px_height_100px_resolution_144dpi.png"
-    )
+    original_image = cv.imread(path)
 
     hsv_image = cv.cvtColor(original_image, cv.COLOR_RGB2HSV)
     black_white_image = cv.inRange(hsv_image, (0, 0, 100), (0, 0, 255))
@@ -15,4 +13,6 @@ def count_cards_in_image(path):
 
 
 if __name__ == "__main__":
-    count_cards_in_image("")
+    count_cards_in_image(
+        "../../tests/data/1_kanban_cards_width_140px_height_100px_resolution_144dpi.png"
+    )
