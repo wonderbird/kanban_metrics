a3143c4332a6144c012f68090acf03bc26b53dcf
Tugbay Atilla <tatilla@igus.net>
2023-09-12 16:40:37 +0200

    refactor: reduce the import statements
    Co-authored-by: Stefan Boos <sboos@igus.net>

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import src
-from src import iterative_metrics
-from iterative_metrics.cumulative_flow_diagram import cumulative_flow_diagram
-from iterative_metrics.count_cards_in_image import count_cards_in_image
+import src.iterative_metrics.cumulative_flow_diagram as diag
+import src.iterative_metrics.count_cards_in_image as img
 
 
 if __name__ == "__main__":
-    count_cards_in_image(
+    img.count_cards_in_image(
         "tests/data/1_kanban_cards_width_140px_height_100px_resolution_144dpi.png"
     )
-    cumulative_flow_diagram()
+    diag.cumulative_flow_diagram()
