aa35ecdf7a56f23bae9f168409521964d46d3251
Stefan Boos <sboos@igus.net>
2023-09-12 20:36:40 +0200

    feat(count_cards): count metrics boxes

--- src/iterative_metrics/count_cards_in_image.py
+++ src/iterative_metrics/count_cards_in_image.py
@@ -5,24 +5,50 @@ import matplotlib.pyplot as plt
 
 def count_cards_in_image(path):
     # read HSV image
-    bgr_input = cv.imread(path)
+    bgr_input = cv.imread(path.__str__())
     hsv_input = cv.cvtColor(bgr_input, cv.COLOR_BGR2HSV)
 
-    # remove non-title decorations - keep only black pixels with a value of 0
-    title_box_with_text = cv.inRange(hsv_input, (0, 0, 0), (0, 0, 1))
+    # identify the metrics area (green box)
+    boxes_with_text = cv.inRange(hsv_input, (35, 50, 50), (70, 255, 255))
 
-    # remove text from title box
+    # remove the text and other small artifacts
+    boxes = boxes_with_text
+
+    # Close cracked borders of boxes
+    kernel = np.ones((3, 3), np.uint8)
+    boxes = cv.dilate(boxes, kernel, iterations=1)
+
+    # Remove small artifacts, e.g. checkboxes, wrong colored pixels
     kernel = np.ones((5, 5), np.uint8)
-    title_box_enlarged = cv.dilate(title_box_with_text, kernel, iterations=1)
-    title_box = cv.erode(title_box_enlarged, kernel, iterations=1)
+    boxes = cv.erode(boxes, kernel, iterations=2)
+
+    # Remove text from inside boxes
+    kernel = np.ones((11, 11), np.uint8)
+    boxes = cv.dilate(boxes, kernel, iterations=1)
+
+    # identify the bounding borders of the filled boxes
+    borders = cv.Canny(boxes, 50, 200)
+
+    # Debugging aid - uncomment to display an image
+    # plt.imshow(borders, cmap="gray")
+    # plt.show()
+
+    # identify outer contours to count number of boxes
+    contours, _ = cv.findContours(borders, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+    number_of_boxes = len(contours)
 
     # display result
-    plt.imshow(title_box, cmap="gray")
-    plt.show()
-    return 0
+    return number_of_boxes
 
 
 if __name__ == "__main__":
-    count_cards_in_image(
-        "../../tests/data/1_kanban_cards_width_140px_height_100px_resolution_144dpi.png"
-    )
+    images = [
+        "../../tests/data/1_kanban_cards_width_140px_height_100px_resolution_144dpi.png",
+        "../../tests/data/2_kanban_cards_1_blocker_width_239px_height_236px_resolution_96dpi.png",
+        "../../tests/data/11_kanban_cards_width_640px_height_480px_resolution_96dpi.png",
+        "../../tests/data/full_kanban_board_width_808px_height_959px_resolution_96dpi.png",
+    ]
+
+    for image in images:
+        number_of_cards = count_cards_in_image(image)
+        print(f"{image} contains {number_of_cards} cards")
--- tests/test_count_cards_in_image.py
+++ tests/test_count_cards_in_image.py
@@ -7,11 +7,10 @@ from iterative_metrics.count_cards_in_image import count_cards_in_image
 
 
 FIXTURE_DIR = Path(__file__).parent.resolve() / "data"
+
+
 FILE_WITH_0_KANBAN_CARDS = (
     "0_kanban_cards_width_140px_height_100px_resolution_144dpi.png"
-)
-FILE_WITH_1_KANBAN_CARDS = (
-    "1_kanban_cards_width_140px_height_100px_resolution_144dpi.png"
 )
 
 
@@ -20,6 +19,41 @@ def test_count_cards_in_image_0(datafiles):
     assert 0 == count_cards_in_image(datafiles / FILE_WITH_0_KANBAN_CARDS)
 
 
+FILE_WITH_1_KANBAN_CARDS = (
+    "1_kanban_cards_width_140px_height_100px_resolution_144dpi.png"
+)
+
+
 @pytest.mark.datafiles(FIXTURE_DIR / FILE_WITH_1_KANBAN_CARDS)
 def test_count_cards_in_image_1(datafiles):
     assert 1 == count_cards_in_image(datafiles / FILE_WITH_1_KANBAN_CARDS)
+
+
+FILE_WITH_2_KANBAN_CARDS = (
+    "2_kanban_cards_1_blocker_width_239px_height_236px_resolution_96dpi.png"
+)
+
+
+@pytest.mark.datafiles(FIXTURE_DIR / FILE_WITH_2_KANBAN_CARDS)
+def test_count_cards_in_image_2(datafiles):
+    assert 2 == count_cards_in_image(datafiles / FILE_WITH_2_KANBAN_CARDS)
+
+
+FILE_WITH_11_KANBAN_CARDS = (
+    "11_kanban_cards_width_640px_height_480px_resolution_96dpi.png"
+)
+
+
+@pytest.mark.datafiles(FIXTURE_DIR / FILE_WITH_11_KANBAN_CARDS)
+def test_count_cards_in_image_11(datafiles):
+    assert 11 == count_cards_in_image(datafiles / FILE_WITH_11_KANBAN_CARDS)
+
+
+FILE_WITH_43_KANBAN_CARDS = (
+    "full_kanban_board_width_808px_height_959px_resolution_96dpi.png"
+)
+
+
+@pytest.mark.datafiles(FIXTURE_DIR / FILE_WITH_43_KANBAN_CARDS)
+def test_count_cards_in_image_43(datafiles):
+    assert 43 == count_cards_in_image(datafiles / FILE_WITH_43_KANBAN_CARDS)
