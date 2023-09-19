bea89c65f3ddce551a402cde44dd91af885e547b
Stefan Boos <kontakt@boos.systems>
2023-09-19 05:31:23 +0200

    feat: find work items by workflow step (single workflow step)

--- src/iterative_metrics/find_work_items_by_workflow_steps.py
+++ src/iterative_metrics/find_work_items_by_workflow_steps.py
@@ -1,2 +1,10 @@
+from iterative_metrics.find_work_items_in_image import find_work_items_in_image
+
+
 def find_work_items_by_workflow_steps(image_file):
-    return []
+    work_items = find_work_items_in_image(image_file)
+
+    if len(work_items) == 0:
+        return []
+    else:
+        return [len(work_items)]
