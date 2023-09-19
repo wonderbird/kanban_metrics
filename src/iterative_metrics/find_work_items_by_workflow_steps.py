2ce89fa7a4b3c611ba54a08e7421dee28b0b2f46
Stefan Boos <kontakt@boos.systems>
2023-09-19 05:36:09 +0200

    feat: find work items by workflow step (two workflow steps, 2 work items in last column)

--- src/iterative_metrics/find_work_items_by_workflow_steps.py
+++ src/iterative_metrics/find_work_items_by_workflow_steps.py
@@ -1,10 +1,14 @@
 from iterative_metrics.find_work_items_in_image import find_work_items_in_image
+from iterative_metrics.find_workflow_steps_in_image import find_workflow_steps_in_image
 
 
 def find_work_items_by_workflow_steps(image_file):
     work_items = find_work_items_in_image(image_file)
+    workflow_steps = find_workflow_steps_in_image(image_file)
 
     if len(work_items) == 0:
         return []
-    else:
-        return [len(work_items)]
+
+    result = [0] * len(workflow_steps)
+    result[-1] = len(work_items)
+    return result
