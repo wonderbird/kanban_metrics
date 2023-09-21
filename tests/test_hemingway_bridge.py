55f3b0a745349bd3b32291b250856fca8bf9fb46
Stefan Boos <kontakt@boos.systems>
2023-09-21 08:04:26 +0200

    docs: refine the big picture event storming

---
+++ tests/test_hemingway_bridge.py
@@ -0,0 +1,6 @@
+# a failing unit test
+def test_hemingway_bridge():
+    # TODO Create bounded contexts or subdomains, then use design level event storming  ...
+    assert (
+        False
+    ), "Create bounded contexts or subdomains, then use design level event storming  ..."
