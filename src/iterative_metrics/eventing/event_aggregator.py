ba49e0084bb9e1496c0a498eb5c5f76cd5b7e556
Stefan Boos <kontakt@boos.systems>
2023-09-25 07:55:23 +0200

    feat: EventAggregator supports different event types

--- src/iterative_metrics/eventing/event_aggregator.py
+++ src/iterative_metrics/eventing/event_aggregator.py
@@ -1,10 +1,23 @@
 class EventAggregator:
     def __init__(self):
-        self.subscribers = []
+        self.subscribers = {}
 
     def subscribe(self, subscriber):
-        self.subscribers.append(subscriber)
+        event_type_name = subscriber.event_type.__name__
 
-    def publish(self):
-        for subscriber in self.subscribers:
+        if not self.is_subscribed(event_type_name):
+            self.subscribers[event_type_name] = []
+
+        self.subscribers[event_type_name].append(subscriber)
+
+    def publish(self, event):
+        event_type_name = event.__class__.__name__
+
+        if not self.is_subscribed(event_type_name):
+            return
+
+        for subscriber in self.subscribers[event_type_name]:
             subscriber.consume()
+
+    def is_subscribed(self, event_type_name):
+        return event_type_name in self.subscribers
--- tests/eventing/test_consume.py
+++ tests/eventing/test_consume.py
@@ -1,19 +1,51 @@
-import pytest
-
 from iterative_metrics.eventing.event_aggregator import EventAggregator
 
 
-class Subscriber:
+def test_when_subscribed_to_event():
+    event_aggregator = EventAggregator()
+
+    subscriber = FirstSubscriber()
+    event_aggregator.subscribe(subscriber)
+
+    event_aggregator.publish(FirstEvent())
+
+    assert subscriber.received_event
+
+
+def test_when_subscribed_to_different_event():
+    event_aggregator = EventAggregator()
+
+    firstSubscriber = FirstSubscriber()
+    event_aggregator.subscribe(firstSubscriber)
+    secondSubscriber = SecondEventSubscriber()
+    event_aggregator.subscribe(secondSubscriber)
+
+    event_aggregator.publish(FirstEvent())
+
+    assert not secondSubscriber.received_event
+
+
+class FirstEvent:
+    pass
+
+
+class FirstSubscriber:
     def __init__(self):
         self.received_event = False
+        self.event_type = FirstEvent
 
     def consume(self):
         self.received_event = True
 
 
-def test_consume():
-    event_aggregator = EventAggregator()
-    subscriber = Subscriber()
-    event_aggregator.subscribe(subscriber)
-    event_aggregator.publish()
-    assert subscriber.received_event
+class SecondEvent:
+    pass
+
+
+class SecondEventSubscriber:
+    def __init__(self):
+        self.received_event = False
+        self.event_type = SecondEvent
+
+    def consume(self):
+        self.received_event = True
