991a4a1cac06851ceedbee851757cc0e00616126
Stefan Boos <kontakt@boos.systems>
2023-09-29 16:40:56 +0200

    refactor: only WorkflowSteps refer to find_workflow_steps_in_image

--- tests/adapters/outbound/test_find_workflow_steps_in_image.py
+++ tests/adapters/outbound/test_find_workflow_steps_in_image.py
@@ -3,9 +3,6 @@ from pathlib import Path
 import cv2
 import pytest
 
-from iterative_metrics.adapters.outbound.find_workflow_steps_in_image import (
-    find_workflow_steps_in_screenshot,
-)
 from iterative_metrics.domain.workflow_steps import WorkflowSteps
 
 FIXTURE_DIR = Path(__file__).parent.parent.parent.resolve() / "data"
@@ -40,9 +37,7 @@ def test_result_should_be_sorted_from_left_to_right():
         FIXTURE_DIR / "2_workflow_steps_width_240px_height_910px_resolution_96dpi.png"
     )
     image = cv2.imread(str(image_path))
-    workflow_steps = find_workflow_steps_in_screenshot(image)
-    assert (
-        workflow_steps[0].bounding_rectangle.x
-        + workflow_steps[0].bounding_rectangle.width
-        < workflow_steps[1].bounding_rectangle.x
-    )
+    workflow_steps = WorkflowSteps.parse_screenshot(image)
+
+    left_x_coordinates = [step.bounding_rectangle.x for step in workflow_steps]
+    assert left_x_coordinates == sorted(left_x_coordinates)
