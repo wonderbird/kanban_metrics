d64f97b201725582bddae8668b5d791a7f2ad0b9
Niklas Rueber <nrueber@igus.net>
2023-10-04 16:04:06 +0200

    feat: metrics of today

--- src/iterative_metrics/adapters/inbound/cumulative_flow_diagram.py
+++ src/iterative_metrics/adapters/inbound/cumulative_flow_diagram.py
@@ -29,6 +29,7 @@ def cumulative_flow_diagram():
         "28.09.23 16:30",
         "29.09.23 16:30",
         "02.10.23 16:00",
+        "04.10.23 16:00", ## 03.10. skipped because of public holiday
     ]
     metrics = {
         "done                ": [
@@ -58,6 +59,7 @@ def cumulative_flow_diagram():
             28,
             28,
             28,
+            28,
         ],
         ########## Data added by Nik ##################
         # 29.09.2023
@@ -73,12 +75,19 @@ def cumulative_flow_diagram():
         # doing other       | hasn't changed
         # -1 agreed cobo    | started on Trivy
         # agreed other      | hasn't changed
+        # --------------------------------------------
+        # 02.10.2023
+        # sharing columns   | (+2 Firefighting Code Review for 966 | AX/BT Problems Taskforce)
+        # doing CoBo        | hasn't changed
+        # doing other       | hasn't changed
+        # agreed cobo       | hasn't changed
+        # agreed other      | hasn't changed
         ##############################################
-        "both sharing columns": [8, 0, 0, 0, 0, 1, 3, 4, 6, 1, 2, 0, 0, 0, 2, 2, 3, 5, 5, 2, 2, 2, 0, 1, 2, 3],
-        "doing cobo          ": [0, 2, 2, 3, 3, 3, 3, 3, 2, 2, 1, 2, 2, 3, 3, 3, 3, 2, 3, 2, 3, 3, 1, 0, 1, 1],
-        "doing other         ": [1, 2, 3, 3, 3, 2, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        "agreed cobo         ": [0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 1],
-        "agreed other        ": [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1],
+        "both sharing columns": [8, 0, 0, 0, 0, 1, 3, 4, 6, 1, 2, 0, 0, 0, 2, 2, 3, 5, 5, 2, 2, 2, 0, 1, 2, 3, 5],
+        "doing cobo          ": [0, 2, 2, 3, 3, 3, 3, 3, 2, 2, 1, 2, 2, 3, 3, 3, 3, 2, 3, 2, 3, 3, 1, 0, 1, 1, 1],
+        "doing other         ": [1, 2, 3, 3, 3, 2, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        "agreed cobo         ": [0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 1, 1],
+        "agreed other        ": [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
     }
 
     show_cumulative_flow_diagram(dates, metrics)
