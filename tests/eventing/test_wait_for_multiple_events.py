77ccc1d0ae2110d9ff669b2ece75d31d0bda859f
Stefan Boos <kontakt@boos.systems>
2023-10-18 05:58:06 +0200

    test: WaitForMultipleEvents waits for arbitrary events

--- tests/eventing/test_wait_for_multiple_events.py
+++ tests/eventing/test_wait_for_multiple_events.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import inject
 
 from iterative_metrics.eventing.consumer import Consumer
@@ -18,20 +20,27 @@ class TestWaitForMultipleEvents:
 
     def test_given_waiting_for_single_event_when_no_event_published(self, mocker):
         mock_callback = mocker.patch.object(self, "callback", autospec=True)
-        WaitForMultipleEvents(self.callback, [AwaitedEvent1()])
+        WaitForMultipleEvents(self.callback, [AwaitedEvent1])
         mock_callback.assert_not_called()
 
     def test_given_waiting_for_single_event_when_event_published(self, mocker):
         mock_callback = mocker.patch.object(self, "callback", autospec=True)
-        WaitForMultipleEvents(self.callback, [AwaitedEvent1()])
+        WaitForMultipleEvents(self.callback, [AwaitedEvent1])
         self._event_aggregator.publish(AwaitedEvent1())
         mock_callback.assert_called_once()
 
     def test_given_waiting_for_two_events_when_only_one_event_published(self, mocker):
         mock_callback = mocker.patch.object(self, "callback", autospec=True)
-        WaitForMultipleEvents(self.callback, [AwaitedEvent1(), AwaitedEvent2()])
+        WaitForMultipleEvents(self.callback, [AwaitedEvent1, AwaitedEvent2])
         self._event_aggregator.publish(AwaitedEvent1())
         mock_callback.assert_not_called()
+
+    def test_given_waiting_for_two_events_when_all_events_published(self, mocker):
+        mock_callback = mocker.patch.object(self, "callback", autospec=True)
+        WaitForMultipleEvents(self.callback, [AwaitedEvent1, AwaitedEvent3])
+        self._event_aggregator.publish(AwaitedEvent1())
+        self._event_aggregator.publish(AwaitedEvent3())
+        mock_callback.assert_called_once()
 
     def callback(self):
         pass
@@ -45,10 +54,14 @@ class AwaitedEvent2:
     pass
 
 
+class AwaitedEvent3:
+    pass
+
+
 class WaitForMultipleEvents:
     event_aggregator = inject.attr(EventAggregator)
 
-    def __init__(self, callback: callable, awaited_events: list) -> None:
+    def __init__(self, callback: callable, awaited_events: List[type]) -> None:
         self.callback = callback
         self.awaited_events = awaited_events
         self.waiting_for = []
@@ -67,11 +80,9 @@ class WaitForMultipleEvents:
             },
         )
 
-        DynamicConsumer(AwaitedEvent1, self)
-        self.waiting_for.append(AwaitedEvent1)
-
-        DynamicConsumer(AwaitedEvent2, self)
-        self.waiting_for.append(AwaitedEvent2)
+        for event_type in self.awaited_events:
+            DynamicConsumer(event_type, self)
+            self.waiting_for.append(event_type)
 
     def consume(self, event: object) -> None:
         if event.__class__ in self.waiting_for:
