b71dda75445982b766016dc8a31c843951cd4ad6
Stefan Boos <kontakt@boos.systems>
2023-10-24 05:43:26 +0200

    refactor: use WaitForMultipleEvents in DetermineBoardStatusˆ

--- src/iterative_metrics/domain/policies/determine_board_status.py
+++ src/iterative_metrics/domain/policies/determine_board_status.py
@@ -7,13 +7,12 @@ from iterative_metrics.domain.events.potential_workflow_steps_found import (
     PotentialWorkflowStepsFound,
 )
 from iterative_metrics.domain.events.work_items_found import WorkItemsFound
-from iterative_metrics.domain.work_items import WorkItems
-from iterative_metrics.domain.workflow_steps import WorkflowSteps
 from iterative_metrics.eventing.consumer import Consumer
 from iterative_metrics.eventing.event_aggregator import EventAggregator
+from iterative_metrics.eventing.event_collection import EventCollection
 
 
-class DetermineBoardStatus:
+class DetermineBoardStatus(Consumer):
     """
     Associate workflow steps with work items.
 
@@ -23,45 +22,16 @@ class DetermineBoardStatus:
     BoardStatusDetermined event.
     """
 
-    event_aggregator = inject.attr(EventAggregator)
+    _event_aggregator = inject.attr(EventAggregator)
 
     def __init__(self) -> None:
-        self._work_items = None
-        self._workflow_steps = None
-        DetermineBoardStatus.PotentialWorkflowStepsFound(self)
-        DetermineBoardStatus.WorkItemsFound(self)
+        super().__init__(EventCollection)
+        self._event_aggregator.subscribe(self)
 
-    def process_workflow_steps(self, workflow_steps: WorkflowSteps) -> None:
-        self._workflow_steps = workflow_steps
-        self.try_associate_workflow_steps_with_work_items()
+    def consume(self, event: EventCollection) -> None:
+        work_items = event.last(WorkItemsFound).work_items
+        workflow_steps = event.last(PotentialWorkflowStepsFound).workflow_steps
 
-    def process_work_items(self, work_items: WorkItems) -> None:
-        self._work_items = work_items
-        self.try_associate_workflow_steps_with_work_items()
+        workflow_steps.associate_with(work_items)
 
-    def try_associate_workflow_steps_with_work_items(self):
-        if self._work_items is not None and self._workflow_steps is not None:
-            self._workflow_steps.associate_with(self._work_items)
-            self.event_aggregator.publish(BoardStatusDetermined(self._workflow_steps))
-
-    class PotentialWorkflowStepsFound(Consumer):
-        event_aggregator = inject.attr(EventAggregator)
-
-        def __init__(self, outer_class) -> None:
-            super().__init__(PotentialWorkflowStepsFound)
-            self.event_aggregator.subscribe(self)
-            self.outer_class = outer_class
-
-        def consume(self, event: PotentialWorkflowStepsFound) -> None:
-            self.outer_class.process_workflow_steps(event.workflow_steps)
-
-    class WorkItemsFound(Consumer):
-        event_aggregator = inject.attr(EventAggregator)
-
-        def __init__(self, outer_class) -> None:
-            super().__init__(WorkItemsFound)
-            self.event_aggregator.subscribe(self)
-            self.outer_class = outer_class
-
-        def consume(self, event: WorkItemsFound) -> None:
-            self.outer_class.process_work_items(event.work_items)
+        self._event_aggregator.publish(BoardStatusDetermined(workflow_steps))
