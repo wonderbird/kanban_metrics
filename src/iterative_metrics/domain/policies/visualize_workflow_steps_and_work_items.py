74923af3d1a0399acdb63cdfaa5b3ebde049c973
Stefan Boos <kontakt@boos.systems>
2023-10-24 05:37:53 +0200

    refactor: use WaitForMultipleEvents in VisualizeWorkflowStepsAndWorkItems

--- src/iterative_metrics/domain/policies/visualize_workflow_steps_and_work_items.py
+++ src/iterative_metrics/domain/policies/visualize_workflow_steps_and_work_items.py
@@ -1,5 +1,4 @@
 import inject
-import numpy as np
 
 from iterative_metrics.adapters.outbound.debug_image import (
     debug_show_rectangles_in_image,
@@ -11,79 +10,31 @@ from iterative_metrics.domain.events.potential_workflow_steps_found import (
     PotentialWorkflowStepsFound,
 )
 from iterative_metrics.domain.events.work_items_found import WorkItemsFound
-from iterative_metrics.domain.work_items import WorkItems
-from iterative_metrics.domain.workflow_steps import WorkflowSteps
 from iterative_metrics.eventing.consumer import Consumer
 from iterative_metrics.eventing.event_aggregator import EventAggregator
+from iterative_metrics.eventing.event_collection import EventCollection
+from iterative_metrics.eventing.wait_for_multiple_events import WaitForMultipleEvents
 
 
-class VisualizeWorkflowStepsAndWorkItems:
+class VisualizeWorkflowStepsAndWorkItems(Consumer):
     """Show work items and workflow steps in screenshot."""
 
+    _event_aggregator = inject.attr(EventAggregator)
+
     def __init__(self) -> None:
-        self._screenshot = None
-        self._work_items = None
-        self._workflow_steps = None
-        VisualizeWorkflowStepsAndWorkItems.BoardScreenshotUpdated(self)
-        VisualizeWorkflowStepsAndWorkItems.PotentialWorkflowStepsFound(self)
-        VisualizeWorkflowStepsAndWorkItems.WorkItemsFound(self)
+        super().__init__(EventCollection)
+        self._event_aggregator.subscribe(self)
+        WaitForMultipleEvents(
+            [BoardScreenshotUpdated, PotentialWorkflowStepsFound, WorkItemsFound]
+        )
 
-    def process_board_screenshot(self, screenshot: np.ndarray) -> None:
-        self._screenshot = screenshot
-        self.try_display_workflow_steps_and_work_items()
+    def consume(self, event: EventCollection) -> None:
+        screenshot = event.last(BoardScreenshotUpdated).screenshot
+        workflow_steps = event.last(PotentialWorkflowStepsFound).workflow_steps
+        work_items = event.last(WorkItemsFound).work_items
 
-    def process_workflow_steps(self, workflow_steps: WorkflowSteps) -> None:
-        self._workflow_steps = workflow_steps
-        self.try_display_workflow_steps_and_work_items()
-
-    def process_work_items(self, work_items: WorkItems) -> None:
-        self._work_items = work_items
-        self.try_display_workflow_steps_and_work_items()
-
-    def try_display_workflow_steps_and_work_items(self):
-        if (
-            self._screenshot is None
-            or self._work_items is None
-            or self._workflow_steps is None
-        ):
-            return
-
-        rectangles = [work_item.bounding_rectangle for work_item in self._work_items]
+        rectangles = [work_item.bounding_rectangle for work_item in work_items]
         rectangles += [
-            workflow_step.bounding_rectangle for workflow_step in self._workflow_steps
+            workflow_step.bounding_rectangle for workflow_step in workflow_steps
         ]
-        debug_show_rectangles_in_image(self._screenshot, rectangles)
-        # TODO refactor this class to remove code duplication in sub classes
-
-    class BoardScreenshotUpdated(Consumer):
-        event_aggregator = inject.attr(EventAggregator)
-
-        def __init__(self, outer_class) -> None:
-            super().__init__(BoardScreenshotUpdated)
-            self.event_aggregator.subscribe(self)
-            self.outer_class = outer_class
-
-        def consume(self, event: BoardScreenshotUpdated) -> None:
-            self.outer_class.process_board_screenshot(event.screenshot)
-
-    class PotentialWorkflowStepsFound(Consumer):
-        event_aggregator = inject.attr(EventAggregator)
-
-        def __init__(self, outer_class) -> None:
-            super().__init__(PotentialWorkflowStepsFound)
-            self.event_aggregator.subscribe(self)
-            self.outer_class = outer_class
-
-        def consume(self, event: PotentialWorkflowStepsFound) -> None:
-            self.outer_class.process_workflow_steps(event.workflow_steps)
-
-    class WorkItemsFound(Consumer):
-        event_aggregator = inject.attr(EventAggregator)
-
-        def __init__(self, outer_class) -> None:
-            super().__init__(WorkItemsFound)
-            self.event_aggregator.subscribe(self)
-            self.outer_class = outer_class
-
-        def consume(self, event: WorkItemsFound) -> None:
-            self.outer_class.process_work_items(event.work_items)
+        debug_show_rectangles_in_image(screenshot, rectangles)
